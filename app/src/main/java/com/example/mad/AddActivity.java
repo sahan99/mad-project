31052e4667e414b11687a9b2f263e7673221add6
sahan99 <sahan.alwis@gmail.com>
2021-09-25 23:49:08 +0530

    hi

--- app/src/main/java/com/example/mad/AddActivity.java
+++ app/src/main/java/com/example/mad/AddActivity.java
@@ -31,8 +31,6 @@ public class AddActivity extends AppCompatActivity {
         contactno = (EditText)findViewById(R.id.txtContactno);
         pic = (EditText)findViewById(R.id.txtImageUrl);
 
-
-
         btnAdd = (Button)findViewById(R.id.btnAdd);
         btnBack = (Button)findViewById(R.id.btnBack);
 
